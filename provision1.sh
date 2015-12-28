a5d49c7c9737820e6a2229bd874810ae88ff1a2d
tkow <t.kusama0613@outlook.jp>
2015-12-28 18:12:23 +0900

    virtualenv pyenv added

--- provision1.sh
+++ provision1.sh
@@ -1,5 +1,5 @@
 sudo yum groupinstall "Development tools"
-sudo yum install -y gcc gcc-c++ zlib-devel bzip2-devel readline-devel openssl-devel openssl ncurses-devel sqlite-devel tk-devel python-devel
+sudo yum install -y gcc gcc-c++ zlib-devel bzip2-devel readline-devel openssl-devel openssl ncurses-devel sqlite-devel tk-devel python-devel git vim
 wget https://www.python.org/ftp/python/3.3.3/Python-3.3.3.tgz
 wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
 tar zxf Python-3.3.3.tgz 
@@ -19,6 +19,8 @@ sudo make
 sudo make altinstall
 sudo ln -s /usr/local/python/lib/libpython3.3m.so.1.0 /lib64/
 sudo ln -s /usr/local/python/bin/python3.3 /usr/local/bin/python3
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 #cd /usr/local/src/gmp
 #wget ftp://ftp.gnu.org/gnu/gmp/gmp-5.0.5.tar.bz2
 #bzip2 -dc gmp-5.0.5.tar.bz2 | tar xvf -
@@ -28,9 +30,23 @@ sudo ln -s /usr/local/python/bin/python3.3 /usr/local/bin/python3
 #sudo make install
 cd ~
 echo "export PATH=$PATH:/usr/local/python">>.bash_profile
+echo 'export PYENV_ROOT="${HOME}/.pyenv"'>>.bash_profile
+echo 'if [ -d "${PYENV_ROOT}" ]; then'>>.bash_profile
+echo '      export PATH=${PYENV_ROOT}/bin:$PATH'>>.bash_profile
+echo '         eval "$(pyenv init -)"'>>.bash_profile
+echo '         eval "$(pyenv virtualenv-init -)"'>>.bash_profile
+echo '        fi'>>.bash_profile
 source .bash_profile
 curl -kL https://bootstrap.pypa.io/get-pip.py | sudo /usr/local/bin/python27
 curl -kL https://bootstrap.pypa.io/get-pip.py | sudo /usr/local/bin/python3
 sudo ln -s /usr/local/python/bin/pip3 /usr/local/bin/pip3
 sudo ln -s /usr/local/python/bin/pip2.7 /usr/local/bin/pip
 
+pyenv install 3.5.1
+pyenv rehash
+#pyenv exec pip install --upgrade pip
+#pyenv rehash
+pyenv install 2.7.11
+pyenv rehash
+#pyenv exec pip install --upgrade pip
+#pyenv rehash
